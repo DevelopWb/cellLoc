2e6be927f5991b85ed88f53c2665bc73b98838e8
unknown <bin_w_ing@foxmail.com>
2021-10-31 23:56:09 +0800

    接收短信

--- app/src/main/java/com/celllocation/newgpsone/MainActivity.java
+++ app/src/main/java/com/celllocation/newgpsone/MainActivity.java
@@ -1,8 +1,16 @@
 package com.celllocation.newgpsone;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
 import android.view.View;
 
 import com.celllocation.R;
@@ -14,6 +22,7 @@ import com.celllocation.newgpsone.functions.persionalLocate.PersionalLocateActiv
 import com.celllocation.newgpsone.functions.wifilocate.WifiLocateActivity;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.juntai.disabled.basecomponent.mvp.BasePresenter;
+import com.juntai.disabled.basecomponent.utils.LogUtil;
 import com.juntai.disabled.basecomponent.utils.ToastUtils;
 
 import java.util.ArrayList;
@@ -27,7 +36,7 @@ public class MainActivity extends BaseAppActivity {
     public final static String HOMEPAGE_MENU_WIFI_LOC = "WIFI定位";
     public final static String HOMEPAGE_MENU_PEOPLE_LOC = "人员定位";
     public final static String HOMEPAGE_MENU_LAT_LNG = "经纬度查询";
-
+    private DynamicReceiver receiverSMS = new DynamicReceiver();
     @Override
     protected BasePresenter createPresenter() {
         return null;
@@ -76,8 +85,38 @@ public class MainActivity extends BaseAppActivity {
                 }
             }
         });
-    }
 
+        IntentFilter filterSMS = new IntentFilter(
+                "android.provider.Telephony.SMS_RECEIVED");
+        registerReceiver(receiverSMS, filterSMS);
+    }
+    // 对收到的短信内容进行提取
+    public class DynamicReceiver extends BroadcastReceiver {
+        public static final String SMS_ACTION = "android.provider.Telephony.SMS_RECEIVED";
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (SMS_ACTION.equals(action)) {
+                Bundle bundle = intent.getExtras();
+                Object messages[] = (Object[]) bundle.get("pdus");
+                final SmsMessage smsMessage[] = new SmsMessage[messages.length];
+
+                for (int n = 0; n < messages.length; n++) {
+                    smsMessage[n] = SmsMessage
+                            .createFromPdu((byte[]) messages[n]);
+                    if (smsMessage[n].getMessageBody().startsWith("#RDW")) {
+                        LogUtil.d("收到短信内容8888888888");
+                        final String num = smsMessage[n].getOriginatingAddress();
+                        //收到短信指令后  开始解析经纬度  然后跳转到定位页面
+
+                    }
+                }
+
+            }
+        }
+
+    }
     /**
      * 获取菜单列表
      *
